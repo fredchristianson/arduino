a553fe065239c21516c3b5706ad25aa38e2d34b6
Fred Christianson <fred.christianson@gmail.comn>
2024-03-26 20:23:17 -0500

    LEDStrip test working

--- projects/HallLights/src/HallApp.cpp
+++ projects/HallLights/src/HallApp.cpp
@@ -2,6 +2,8 @@
 #include "./HallApp.h"
 #include "fclib/Hardware.h"
 #include "fclib/Timer.h"
+#include "fclib/LedStrip.h"
+#include "fclib/Render.h"
 
 using namespace FCLIB;
 
@@ -22,7 +24,7 @@ void HallApp::setupComplete()
                      { this->doTask(); });
 
     button.setPin(4);
-    led.setPin(5);
+    // ledStrip.setPin(2);
 
     listener.handle(EventType::PRESS_EVENT, &button, [this](Event *event)
                     { this->onButtonChange(event); });
@@ -30,9 +32,11 @@ void HallApp::setupComplete()
     String deviceName = getConfig()->get("device_name", "FCLIB Device");
     ha = new HA::HomeAssistant(getNetwork()->getMqtt());
     haDevice = ha->createDevice(deviceName.c_str());
-    haButton = new HA::Switch(haDevice, &button, "Toggle Switch");
-    haLed = new HA::Led(haDevice, &led);
+    // haButton = new HA::Switch(haDevice, &button, "Toggle Switch");
+    // haLed = new HA::Led(haDevice, &led);
 
+    NeoPixelStrip *strip = new NeoPixelStrip(5, 15, 100);
+    this->ledStrip = strip;
     ha->publishConfig();
 
     Task::repeat([this]()
@@ -40,8 +44,22 @@ void HallApp::setupComplete()
         ->delaySeconds(60);
 }
 
+int r = 0;
+int g = 0;
+int b = 0;
+
 void HallApp::doTask()
 {
+    ledStrip->clear();
+    log.debug("render");
+    RGBRenderer rgb(r, g, b);
+    r += 1;
+    g += 2;
+    b += 3;
+    rgb.draw(*this->ledStrip);
+    // HSVRenderer hsv(100, 100, 50);
+    // hsv.draw(*this->ledStrip);
+    ledStrip->show();
 }
 
 void HallApp::onButtonChange(Event *event)
